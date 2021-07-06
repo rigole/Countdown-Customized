da1531534972ef4b33f3f2f17564675e6f14fbb4
rigole <foplacide@gmail.com>
2021-07-06 19:03:18 +0100

    getting the value of titlecountdown and the chosen date

--- script.js
+++ script.js
@@ -2,16 +2,19 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
-// set Date Input Min with Today's Date
+let countdownTitle = '';
+let countdownDate = '';
 
+// set Date Input Min with Today's Date
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
 
 // Take Values from Form Input
-
 function updateCountdown(e) {
   e.preventDefault();
-  console.log(e);
+  countdownTitle = e.srcElement[0].value;
+  countdownDate = e.srcElement[1].value;
+  console.log(countdownTitle, countdownDate );
 }
 // Event Listeners
 countdownForm.addEventListener('submit', updateCountdown);
