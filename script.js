ed548dc6d7b8f1f2754d848b689951df6bd8bbb3
rigole <foplacide@gmail.com>
2021-07-06 18:47:02 +0100

    fixing the minimum date of today in the date picker

---
+++ script.js
@@ -0,0 +1,9 @@
+const inputContainer = document.getElementById('input-container');
+const countdownForm = document.getElementById('countdownForm');
+const dateEl = document.getElementById('date-picker');
+
+// set Date Input Min with Today's Date
+
+const today = new Date().toISOString().split('T')[0];
+dateEl.setAttribute('min', today);
+
