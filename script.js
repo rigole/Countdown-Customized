e81dbc448298f141ac1782a9522f85ee2a04ad95
rigole <foplacide@gmail.com>
2021-07-07 23:28:00 +0100

    splitting time value in days, hours, minutes ans seconds

--- script.js
+++ script.js
@@ -2,12 +2,36 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+const countdownEl = document.getElementById('countdown');
+const countdownEltitle = document.getElementById('countdown-title');
+const  countdownBtn = document.getElementById('countdown-button');
+const timeElements = document.querySelectorAll('span');
+
+
 let countdownTitle = '';
 let countdownDate = '';
+let countdownValue = Date;
 
+
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
 // set Date Input Min with Today's Date
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+function updateDOM(){
+  const now = new Date().getTime();
+  const distance = countdownValue - now;
+
+  const days = Math.floor(distance / day);
+  const hours = Math.floor(( distance % day) / hour);
+  const minutes = Math.floor(( distance % hour) / minute);
+  const seconds = Math.floor(( distance % minute) / second);
+
+}
+
 
 // Take Values from Form Input
 function updateCountdown(e) {
@@ -15,6 +39,12 @@ function updateCountdown(e) {
   countdownTitle = e.srcElement[0].value;
   countdownDate = e.srcElement[1].value;
   console.log(countdownTitle, countdownDate );
+  // Get number version of current Date , updateDom
+  countdownValue =  new Date(countdownDate).getTime();
+  console.log('countdown value:', countdownValue);
+
+  updateDOM();
+
 }
 // Event Listeners
 countdownForm.addEventListener('submit', updateCountdown);
